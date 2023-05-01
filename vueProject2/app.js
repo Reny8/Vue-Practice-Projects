e9fe7c50bfc357b1bbadf55a20376caa14fe2bea
Reny Reyes <98413865+Reny8@users.noreply.github.com>
2023-05-01 11:46:20 -0400

    Add accessing the data items in a method example

--- vueProject2/app.js
+++ vueProject2/app.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: 'Finish the course and learn Vue!',
+      goalA: 'Finish the course and learn Vue!',
+      goalB: 'Master Vue and build amazing apps!',
       link: 'https://vuejs.org',
     };
   },
@@ -9,9 +10,9 @@ const app = Vue.createApp({
     outputGoal() {
       const randomNumber = Math.random();
       if (randomNumber < 0.5) {
-        return 'Learn Vue!';
+        return this.goalA;
       }
-      return 'Master Vue!';
+      return this.goalB;
     },
   },
 });
